4dc5157785c69a4a60e974e407fb3524e35c356f
Sonny Liu <sliu2017@umd.edu>
2021-03-20 03:37:24 -0400

    Finished About Section and minor adjustments

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const About = (
-    <div>
-        
+    <div className="about-body-container">
+        <img src={`${process.env.PUBLIC_URL}/images/about/phantom-walk.gif`} alt="Spooky Walk" className="about-body-foreground"></img>
+        <div className="about-body-caption">
+            <h1>Hi, I'm Sonny Liu</h1>
+            <p>IAmA:</p>
+            <p>Dreamer, Programmer, Gamer, Developer, Writer, and Soon-to-be UMD Graduate.</p>
+        </div>
+        <button type="button" className="about-body-resume">
+            <a href={`${process.env.PUBLIC_URL}/resumedl/SonnyLiu_ResumeVer1.pdf`} download="SonnyLiuResume">
+                <i className="fa fa-download"></i> Résumé
+            </a>
+        </button>
     </div>
 );
 
--- src/components/Home/Cover.js
+++ src/components/Home/Cover.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cover = (
     <div>
-        <img src={`${process.env.PUBLIC_URL}/images/cover/cover-img.png`} alt="Not quite blond..." className="cover-image" />
+        <img src={`${process.env.PUBLIC_URL}/images/cover/cover-pixelated-img.png`} alt="Not quite blond" className="cover-image" />
     </div>
 );
 
