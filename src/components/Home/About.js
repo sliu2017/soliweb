7d49a61b98c7988ad95f2717e83527411a6f02d8
Sonso <32463988+sliu2017@users.noreply.github.com>
2021-06-02 21:30:51 -0400

    Update About.js

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -10,14 +10,14 @@ const About = (
         <div className="about-body-caption">
             <span className="about-body-caption-first">Hi, I'm Sonny Liu</span><br />
             <span className="about-body-caption-second">IAmA: </span><br />
-            <span>Dreamer, Programmer, Gamer, Developer, Writer, and Soon-to-be UMD Graduate.</span>
-            {/* ["Dreamer", "Programmer", "Gamer", "Developer", "Writer", "Soon-to-be UMD Graduate"] */}
+            <span>Dreamer, Programmer, Gamer, Developer, Writer, and UMD Graduate.</span>
+            {/* ["Dreamer", "Programmer", "Gamer", "Developer", "Writer", "UMD Graduate"] */}
         </div>
 
         <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1uw8pi5cshTHRfmgf_lDWZjOG_UJnTG35/view?usp=sharing"
+            href="https://drive.google.com/file/d/1qkeizJZMJhKjYjAsMRjtK5wSLpnEDL7i/view?usp=sharing"
             download="SonnyLiuResume"
         >
             <button type="button" className="about-body-resume">
